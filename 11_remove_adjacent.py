bbcb5cfa74337cf1f5703e235b1a992d8c8ed0f4
Jpdsbarbosa <jpdsbarbosa@inf.ufpel.edu.br>
2021-07-23 01:10:06 -0300

    Solução 1.0 do problema 11_remove_adjacent.py

--- 11_remove_adjacent.py
+++ 11_remove_adjacent.py
@@ -10,8 +10,7 @@ Irá retornar: [1, 2, 3]
 
 def remove_adjacent(nums):
     # +++ SUA SOLUÇÃO +++
-    return
-
+    return [nums[i] for i in range(len(nums)) if nums[i] != nums[i - 1] or i == 0]
 
 # --- Daqui para baixo são apenas códigos auxiliáries de teste. ---
 
